b0cde9a4472ae869ad8d27e48311ee26de78b613
facundoolano <facundo.olano@gmail.com>
2024-02-14 16:28:48 -0300

    more cleanup

--- commands/commands.go
+++ commands/commands.go
@@ -44,68 +44,69 @@ func buildTarget(site *site.Site, minify bool, htmlReload bool) error {
 
 	// walk the source directory, creating directories and files at the target dir
 	return filepath.WalkDir(SRC_DIR, func(path string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
 		subpath, _ := filepath.Rel(SRC_DIR, path)
 		targetPath := filepath.Join(TARGET_DIR, subpath)
 
 		if entry.IsDir() {
-			os.MkdirAll(targetPath, FILE_RW_MODE)
-		} else {
+			return os.MkdirAll(targetPath, FILE_RW_MODE)
+		}
 
-			var content string
-			var contentReader io.Reader
-			var err error
+		var content string
+		var contentReader io.Reader
 
+		if templ, ok := site.Templates[path]; ok {
 			// if a liquid template was found at source, render it
-			if templ, ok := site.Templates[path]; ok {
-				content, err = site.Render(templ)
+			content, err = site.Render(templ)
+			if err != nil {
+				return err
+			}
+			contentReader = strings.NewReader(content)
+
+		} else {
+			// otherwise treat the file as static, open for copying as is
+			srcFile, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer srcFile.Close()
+			contentReader = srcFile
+		}
+
+		// if it's org or markdown, export to html
+		switch filepath.Ext(targetPath) {
+		case ".org":
+			{
+				doc := org.New().Parse(contentReader, path)
+				content, err = doc.Write(org.NewHTMLWriter())
 				if err != nil {
 					return err
 				}
 				contentReader = strings.NewReader(content)
-
-			} else {
-				// FIXME read contents of the file to be copied to target
-				srcFile, err := os.Open(path)
-				if err != nil {
-					return err
-				}
-				defer srcFile.Close()
-				contentReader = srcFile
+				targetPath = strings.TrimSuffix(targetPath, ".org") + ".html"
 			}
-
-			// if it's org or markdown, export to html
-			switch filepath.Ext(targetPath) {
-			case ".org":
-				{
-					doc := org.New().Parse(contentReader, path)
-					content, err = doc.Write(org.NewHTMLWriter())
-					if err != nil {
-						return err
-					}
-					contentReader = strings.NewReader(content)
-					targetPath = strings.TrimSuffix(targetPath, ".org") + ".html"
-				}
-			case ".md":
-				{
-					// TODO parse markedown
-					targetPath = strings.TrimSuffix(targetPath, ".md") + ".html"
-				}
+		case ".md":
+			{
+				// TODO parse markedown
+				targetPath = strings.TrimSuffix(targetPath, ".md") + ".html"
 			}
-
-			ext := filepath.Ext(targetPath)
-			if htmlReload && ext == ".html" {
-				// TODO inject live reload snippet
-			}
-
-			if minify && (ext == ".html" || ext == ".css" || ext == ".js") {
-				// TODO minify output
-			}
-
-			// FIXME do the contents copying
-			// // write the file contents over to target at the same location
-			fmt.Println("writing", targetPath)
-			copyToFile(targetPath, contentReader)
 		}
+
+		ext := filepath.Ext(targetPath)
+		if htmlReload && ext == ".html" {
+			// TODO inject live reload snippet
+		}
+
+		if minify && (ext == ".html" || ext == ".css" || ext == ".js") {
+			// TODO minify output
+		}
+
+		// FIXME do the contents copying
+		// // write the file contents over to target at the same location
+		fmt.Println("writing", targetPath)
+		copyToFile(targetPath, contentReader)
 
 		return nil
 	})
