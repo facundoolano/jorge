f224078a7c4d955b6d6f02a09c9b0d2d2ac09145
facundoolano <facundo.olano@gmail.com>
2024-02-23 19:21:05 -0300

    add versions and aliases

--- main.go
+++ main.go
@@ -12,16 +12,18 @@ import (
 var cli struct {
 	Init struct {
 		ProjectDir string `arg:"" name:"path" help:"directory where to initialize the website project."`
-	} `cmd:"" help:"Initialize a new website project."`
+	} `cmd:"" help:"Initialize a new website project." aliases:"i"`
 	Build struct {
 		ProjectDir string `arg:"" name:"path" optional:"" default:"." help:"path to the website project to build."`
-	} `cmd:"" help:"Build a website project."`
+	} `cmd:"" help:"Build a website project." aliases:"b"`
 	Post struct {
 		Title string `arg:"" optional:""`
-	} `cmd:"" help:"Initialize a new post template file." help:"title of the new post."`
+	} `cmd:"" help:"Initialize a new post template file." help:"title of the new post." aliases:"p"`
 	Serve struct {
 		ProjectDir string `arg:"" name:"path" optional:"" default:"." help:"path to the website project to serve."`
-	} `cmd:"" help:"Run a local server for the website."`
+	} `cmd:"" help:"Run a local server for the website." aliases:"s"`
+	Version     struct{} `cmd:"" help:"print version information" aliases:"v"`
+	VersionFlag bool     `name:"version" short:"v"`
 }
 
 func main() {
@@ -35,7 +37,12 @@ func main() {
 
 // FIXME try to reduce duplication/boilerplate
 func run() error {
-	ctx := kong.Parse(&cli, kong.UsageOnError())
+	ctx := kong.Parse(&cli, kong.UsageOnError(), kong.HelpOptions{FlagsLast: true})
+	if cli.VersionFlag {
+		printVersion()
+		return nil
+	}
+
 	switch ctx.Command() {
 	case "init <path>":
 		return commands.Init(cli.Init.ProjectDir)
@@ -65,7 +72,15 @@ func run() error {
 			return err
 		}
 		return commands.Serve(config)
+	case "version":
+		printVersion()
+		return nil
 	default:
 		return fmt.Errorf("unexpected input %s", ctx.Command())
 	}
+}
+
+func printVersion() {
+	// FIXME set this automatically when tagging in git
+	fmt.Println("jorge v0.1.2")
 }
