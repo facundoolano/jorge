936332373c99bab597099e0b87550c7f5a171868
facundoolano <facundo.olano@gmail.com>
2024-02-24 12:01:42 -0300

    replace command switch with Run methods

--- main.go
+++ main.go
@@ -1,75 +1,76 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/alecthomas/kong"
 	"github.com/facundoolano/jorge/commands"
 	"github.com/facundoolano/jorge/config"
 )
 
+// TODO move to commands/init
+type Init struct {
+	ProjectDir string `arg:"" name:"path" help:"directory where to initialize the website project."`
+}
+
+func (cmd *Init) Run(ctx *kong.Context) error {
+	return commands.Init(cmd.ProjectDir)
+}
+
+type Build struct {
+	ProjectDir string `arg:"" name:"path" optional:"" default:"." help:"path to the website project to build."`
+}
+
+func (cmd *Build) Run(ctx *kong.Context) error {
+	config, err := config.Load(cmd.ProjectDir)
+	if err != nil {
+		return err
+	}
+	return commands.Build(config)
+}
+
+type Post struct {
+	Title string `arg:"" optional:""`
+}
+
+func (cmd *Post) Run(ctx *kong.Context) error {
+	title := cmd.Title
+	if title == "" {
+		title = commands.Prompt("title")
+	}
+	config, err := config.Load(".")
+	if err != nil {
+		return err
+	}
+	return commands.Post(config, title)
+}
+
+type Serve struct {
+	ProjectDir string `arg:"" name:"path" optional:"" default:"." help:"path to the website project to serve."`
+}
+
+func (cmd *Serve) Run(ctx *kong.Context) error {
+	// FIXME add flags
+	config, err := config.LoadDev(cmd.ProjectDir, "localhost", 4001, true)
+	if err != nil {
+		return err
+	}
+	return commands.Serve(config)
+}
+
 var cli struct {
-	Init struct {
-		ProjectDir string `arg:"" name:"path" help:"directory where to initialize the website project."`
-	} `cmd:"" help:"Initialize a new website project." aliases:"i"`
-	Build struct {
-		ProjectDir string `arg:"" name:"path" optional:"" default:"." help:"path to the website project to build."`
-	} `cmd:"" help:"Build a website project." aliases:"b"`
-	Post struct {
-		Title string `arg:"" optional:""`
-	} `cmd:"" help:"Initialize a new post template file." help:"title of the new post." aliases:"p"`
-	Serve struct {
-		ProjectDir string `arg:"" name:"path" optional:"" default:"." help:"path to the website project to serve."`
-	} `cmd:"" help:"Run a local server for the website." aliases:"s"`
+	Init    Init             `cmd:"" help:"Initialize a new website project." aliases:"i"`
+	Build   Build            `cmd:"" help:"Build a website project." aliases:"b"`
+	Post    Post             `cmd:"" help:"Initialize a new post template file." help:"title of the new post." aliases:"p"`
+	Serve   Serve            `cmd:"" help:"Run a local server for the website." aliases:"s"`
 	Version kong.VersionFlag `short:"v"`
 }
 
 func main() {
-	err := run()
-
-	if err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
-	}
-}
-
-// FIXME try to reduce duplication/boilerplate
-func run() error {
-	// FIXME replace version automatically with git tag
-	ctx := kong.Parse(&cli, kong.UsageOnError(), kong.HelpOptions{FlagsLast: true},
-		kong.Vars{"version": "jorge v.0.1.2"})
-
-	switch ctx.Command() {
-	case "init <path>":
-		return commands.Init(cli.Init.ProjectDir)
-	case "build", "build <path>":
-		config, err := config.Load(cli.Build.ProjectDir)
-		if err != nil {
-			return err
-		}
-		return commands.Build(config)
-	case "post <title>":
-		config, err := config.Load(".")
-		if err != nil {
-			return err
-		}
-		return commands.Post(config, cli.Post.Title)
-	case "post":
-		title := commands.Prompt("title")
-		config, err := config.Load(".")
-		if err != nil {
-			return err
-		}
-		return commands.Post(config, title)
-	case "serve", "serve <path>":
-		// FIXME add flags
-		config, err := config.LoadDev(cli.Serve.ProjectDir, "localhost", 4001, true)
-		if err != nil {
-			return err
-		}
-		return commands.Serve(config)
-	default:
-		return fmt.Errorf("unexpected input %s", ctx.Command())
-	}
+	ctx := kong.Parse(
+		&cli,
+		kong.UsageOnError(),
+		kong.HelpOptions{FlagsLast: true},
+		kong.Vars{"version": "jorge v.0.1.2"},
+	)
+	err := ctx.Run()
+	ctx.FatalIfErrorf(err)
 }
