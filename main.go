81aa742c57a6b0d2d5990b5f650f0be0efab10b6
facundoolano <facundo.olano@gmail.com>
2024-03-07 12:50:24 -0300

    v0.4.1

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 		&cli,
 		kong.UsageOnError(),
 		kong.HelpOptions{FlagsLast: true},
-		kong.Vars{"version": "jorge v0.4.0"},
+		kong.Vars{"version": "jorge v0.4.1"},
 	)
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
